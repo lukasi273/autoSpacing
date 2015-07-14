b5d5156674e5a103313018b2d9a48e17d8d22216
lukasi273 <lukasani2273@gmail.com>
2015-07-14 16:57:50 +0200

    near done

--- autoSpacing/Program.cs
+++ autoSpacing/Program.cs
@@ -3,6 +3,7 @@
     #region
 
     using System;
+    using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
 
@@ -12,13 +13,23 @@
     using SFXLibrary;
     using SFXLibrary.Extensions.SharpDX;
 
-    using Color = SharpDX.Color;
-    using Font = SharpDX.Direct3D9.Font;
+    using SharpDX;
+    using SharpDX.Direct3D9;
 
     #endregion
 
     internal class Program
     {
+        private const float CheckInterval = 1 * 100;
+
+        private static readonly Dictionary<int, int> R1 = new Dictionary<int, int>();
+
+        private static readonly Dictionary<int, int> R2 = new Dictionary<int, int>();
+
+        private static readonly Dictionary<int, int> R3 = new Dictionary<int, int>();
+
+        private static float lastCheck;
+
         private static bool playerAa;
 
         private static bool targetAa;
@@ -31,6 +42,26 @@
 
         private static Font autoSpacingText;
 
+        public static void OnUpdate(EventArgs args)
+        {
+            if (lastCheck + CheckInterval > Environment.TickCount)
+            {
+                return;
+            }
+
+            lastCheck = Environment.TickCount;
+            foreach (var enemy in GameObjects.EnemyHeroes.Where(e => !e.IsDead && e.IsVisible))
+            {
+                R1[enemy.NetworkId] =
+                    (int)
+                    (Math.Round((enemy.Distance(player.Position) - enemy.AttackRange - player.BoundingRadius) / 5f) * 5);
+                R2[enemy.NetworkId] =
+                    (int)
+                    (Math.Round((player.Distance(enemy.Position) - player.AttackRange - enemy.BoundingRadius) / 5f) * 5);
+                R3[enemy.NetworkId] = R2[enemy.NetworkId] - R1[enemy.NetworkId];
+            }
+        }
+
         private static void Main()
         {
             GameObjects.Initialize();
@@ -40,13 +71,14 @@
         private static void OnLoad(EventArgs args)
         {
             player = ObjectManager.Player;
-            
+
             menu = new Menu("autoSpacing", "menu", true);
 
-            menu.AddItem(new MenuItem("autoSpacing.PlayerAA", "Player AA range").SetValue(true));
             menu.AddItem(new MenuItem("autoSpacing.TargetAA", "Target AA range").SetValue(true));
+            menu.AddItem(new MenuItem("autoSpacing.PlayerAA", "Player AA range").SetValue(true));
             menu.AddItem(new MenuItem("autoSpacing.Comparison", "Comparison between the two").SetValue(true));
-            menu.AddItem(new MenuItem("autoSpacing.FontSize", "Font size").SetValue(new Slider(13, 3, 30)));
+            menu.AddItem(
+                new MenuItem("autoSpacing.FontSize", "Font size (F5 required)").SetValue(new Slider(13, 3, 30)));
 
             var whitelistMenu = menu.AddSubMenu(new Menu("Pick a target", "autoSpacing.Whitelist"));
             foreach (var enemy in GameObjects.EnemyHeroes)
@@ -59,6 +91,7 @@
             menu.AddToMainMenu();
 
             Drawing.OnEndScene += OnDrawingEndScene;
+            Game.OnUpdate += OnUpdate;
         }
 
         private static void OnDrawingEndScene(EventArgs args)
@@ -68,8 +101,8 @@
                 return;
             }
 
-            playerAa = menu.Item("autoSpacing.PlayerAA").GetValue<bool>();
-            targetAa = menu.Item("autoSpacing.TargetAA").GetValue<bool>();
+            playerAa = menu.Item("autoSpacing.TargetAA").GetValue<bool>();
+            targetAa = menu.Item("autoSpacing.PlayerAA").GetValue<bool>();
             comparison = menu.Item("autoSpacing.Comparison").GetValue<bool>();
             {
                 foreach (var enemy in
@@ -79,34 +112,27 @@
                         && menu.Item("autoSpacing.Whitelist" + e.ChampionName).GetValue<bool>()
                         && e.Distance(player) < 2000f))
                 {
-                    var r1 =
-                        Math.Round((enemy.Distance(player.Position) - enemy.AttackRange - player.BoundingRadius) / 5f)
-                        * 5;
-                    var r2 =
-                        Math.Round((player.Distance(enemy.Position) - player.AttackRange - enemy.BoundingRadius) / 5f)
-                        * 5;
-                    var r3 = r2 - r1;
                     if (playerAa)
                     {
                         autoSpacingText.DrawTextCentered(
-                            r1.ToString(CultureInfo.InvariantCulture),
-                            Drawing.WorldToScreen(player.Position),
+                            R1[enemy.NetworkId].ToString(CultureInfo.InvariantCulture), 
+                            Drawing.WorldToScreen((Vector3)player.Position.To2D()), 
                             Color.White);
                     }
 
                     if (targetAa)
                     {
                         autoSpacingText.DrawTextCentered(
-                            r2.ToString(CultureInfo.InvariantCulture),
-                            Drawing.WorldToScreen(player.Position),
+                            R2[enemy.NetworkId].ToString(CultureInfo.InvariantCulture), 
+                            Drawing.WorldToScreen((Vector3)player.Position.To2D()), 
                             Color.White);
                     }
 
                     if (comparison)
                     {
                         autoSpacingText.DrawTextCentered(
-                            r3.ToString(CultureInfo.InvariantCulture),
-                            Drawing.WorldToScreen(player.Position),
+                            R3[enemy.NetworkId].ToString(CultureInfo.InvariantCulture), 
+                            Drawing.WorldToScreen((Vector3)player.Position.To2D()), 
                             Color.White);
                     }
                 }
